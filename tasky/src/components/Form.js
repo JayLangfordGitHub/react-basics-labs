90f221efa781f4e303b4678973e85a9d5473db5e
JayLangfordGitHub <jaylangfordprf@gmail.com>
2023-10-09 13:29:59 +0100

    Week 3 Exercises done

--- tasky/src/components/Form.js
+++ tasky/src/components/Form.js
@@ -1,27 +1,86 @@
 import React from "react";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
 
 const AddTaskForm = (props) => {
 
   return (
+    <Box
+    component="form"
+    sx={{
+      '& .MuiOutlinedInput-root': { m: 1, width: '30ch' },
+    }}
+    onSubmit={props.submit}
+  >
     <div>
-      <form onSubmit={props.submit}>
-      <label>
-        Task title:
-        <input type="text" name="title" required onChange={(event) => props.change(event)} />
-    </label>
-    <br />
-    <label>
-        Due date:
-        <input type="date" name="deadline" required onChange={(event) => props.change(event)} />
-    </label>
-    <br />
-    <label>
-        Details:
-        <input type="text" name="description" onChange={(event) => props.change(event)} />
-    </label>
-        <input type="submit" value="Submit" />
-        </form>
+      <TextField
+        required
+        id="outlined-required"
+        name="title"
+        label="Task Title"
+        InputLabelProps={{ shrink: true }}
+        onChange={(event) => props.change(event)}
+      />
     </div>
+    <div>
+      <TextField
+        required
+        name="deadline"
+        label="Deadline"
+        InputLabelProps={{ shrink: true }}
+        type="date"
+        onChange={(event) => props.change(event)}
+      />
+    </div>
+    <div>
+      <TextField
+        name="description"
+        id="outlined-multiline-static"
+        label="Task Details"
+        InputLabelProps={{ shrink: true }}
+        multiline
+        rows={4}
+        onChange={(event) => props.change(event)}
+      />
+    </div>
+    <div>
+        <FormControl fullWidth variant="outlined">
+          <InputLabel id="priority-label">Priority</InputLabel>
+          <Select
+            labelId="priority-label"
+            id="priority"
+            name="priority"
+            label="Priority"
+            onChange={(event) => props.change(event)}
+          >
+            <MenuItem value={"low"}>Low</MenuItem>
+            <MenuItem value={"medium"}>Medium</MenuItem>
+            <MenuItem value={"high"}>High</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+    <div>
+      <Button 
+        type="submit"
+        variant="contained"
+        color="primary"
+        sx={{
+          m: 1,
+          p: 1,
+          width: '95%'
+        }}
+      >
+        Add Task
+      </Button>
+    </div>
+
+
+    </Box>
   )
 };
 
